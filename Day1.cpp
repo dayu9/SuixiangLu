98d707cfdc8e5efb40b8222c85a0a9284366e5e5
shepherd <66990765+dayu9@users.noreply.github.com>
2024-09-27 00:59:39 +0800

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -11,10 +11,10 @@ class Solution_704
 	// 注意1：
 		//	这道题的需要注意点在于边界的选取，左右边界更新时的新值
 		//  与终止条件的==
-		//  最核心的办法，循环不变量，参考下面注释
+		//  最核心的办法，循环不变量(变化规律是不变的 ——> 区间范围规律是不变的)
 
 	// 注意2：
-		// 为什么用 left + (right-left)/2;————防止越界
+		// 为什么用 left + (right-left)/2————防止越界
 public:
 	int search1(vector<int>& arr, int value)
 	{
@@ -22,14 +22,12 @@ public:
 		int right = arr.size()-1;
 		while(left<=right)
 		{
-			// 循环不变量：
-            // nums[left-1] < target
-            // nums[right+1] >= target
+
 			int mid = left + (right-left)/2;
-			
 			if(arr[mid]==value ) return mid;
-			else if(arr[mid]<value) left = mid+1;
-			else right = mid-1;
+			
+			else if(arr[mid]<value) left = mid+1; //循环：每次更新的区间[mid+1, right ],因为arr[mid]肯定不等
+			else right = mid-1;					  //循环：每次更新的区间[left , right-1],因为arr[right]肯定不等 
 		}
 		return -1;
 	}
@@ -44,8 +42,9 @@ public:
 			int mid = left + (right-left)/2;
 			if(arr[mid]==value ) return mid;
 			
-			else if(arr[mid]<value) left = mid+1;
-			else right = mid;
+			//循环不变量
+			else if(arr[mid]<value) left = mid+1;//循环：每次更新的区间[mid+1,right),因为arr[mid]肯定不等
+			else right = mid;                    //循环：每次更新的区间[left,right), 因为arr[right] 取不到
 		}
 		return -1;
 	}
@@ -60,4 +59,3 @@ int main()
 	cout<< result<<endl;
 	return 0;
 }
-
